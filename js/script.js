63ca6c408d7748f7a135e7b8ae6011e3c1a923bb
monika-ustjaniuk <monikaustjaniuk@hotmail.com>
2022-04-07 19:48:29 +0200

    zmiany w js-walidacja

--- js/script.js
+++ js/script.js
@@ -9,44 +9,48 @@ const validate = (event) => {
     let errors = document.getElementById('error-name');
     let errorsEmail = document.getElementById('error-email');
     let errorsPhone = document.getElementById('error-phone');
+    let errorsCity = document.getElementById('error-city')
     
-    
-    let placeField = false
-
-   
-    
-        errors.innerHTML = '';
+         errors.innerHTML = '';
    if (name.value.trim() === ''){
+       name.classList.add('error');
         let errorLi = document.createElement('li');
         errorLi.innerText = 'Wpisz imię';
         errors.appendChild(errorLi);
-    } 
+    } else {name.classList.remove('error');}
 
         errorsEmail.innerHTML = '';
     if (email.value.trim() === '' || /* !email.value.includes('@')  */ !email.value.match(/^[^@\s]+@([^@\s])+$/)){
+        email.classList.add('error');
         let errorLi = document.createElement('li');
         errorLi.innerText = 'example@gmail.com';
         errorsEmail.appendChild(errorLi);
-    }
+    } else {email.classList.remove('error');}
 
         errorsPhone.innerHTML ='';
     if (phone.value.trim() === '' || phone.value.length < 6)  {
+        phone.classList.add('error');
         let errorLi = document.createElement('li');
         errorLi.innerText = 'Wpisz numer telefonu';
         errorsPhone.appendChild(errorLi);
-        
-    }
+    } else {phone.classList.remove('error');}
 
-    if (city.value === '') {
-        placeField = false;
-        city.classList.add('red');
+        errorsCity.innerHTML='';
+    if (city.value === "") {
+         city.classList.add('error');
+        let errorLi = document.createElement('li'); 
+        errorLi.innerText = 'Wybierz miasto';
+        errorsCity.appendChild(errorLi);
+    } else {city.classList.remove('error');}
+     
+    if (isFormValid === false){
+        event.preventDefault();
     } else {
-        placeField = true;
-        city.classList.remove('red');
+        newsletterForm.submit();
     }
 
-   event.preventDefault()
 }
+
 
 function isNumber(e)
 {
@@ -59,8 +63,29 @@ return false;
 return true;
 }  
    
- 
+ function isFormValid(){
+    const inputContainer = newsletterForm.querySelectorAll('.input-group');
+    const selectGroup = newsletterForm.querySelector('.select-group');
+    let result = true;
+    inputContainer.forEach((container) => {
+        if (container.classList.contains('error')){
+            result = false
+        }
+    });
+    if (selectGroup.classList.contains('error')){
+        result = false;
+    }
+
+    return result;
+ }
+
+  newsletterForm.addEventListener('submit',validate);
 
 
-
-newsletterForm.addEventListener('submit', validate);
+/*  newsletterForm.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    validate();
+    if (isFormValid() === true){
+        newsletterForm.submit();
+    }
+ }); */
